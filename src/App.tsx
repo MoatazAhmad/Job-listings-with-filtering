d6efb815f7a14049e7f9b5b2efc741bfe4d157b6
Moataz Sapry <alfr3on@gmail.com>
2025-04-18 05:39:45 +0200

    chore: initialize project with design assets and data  - Added design images to the 'design/' directory - Included development images in the 'assets/images/' directory - Placed 'data.json' in the 'public/' directory for static access

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-        <h1 className='bg-amber-300'>heloworld</h1>
+      <h1>starting</h1>
+      <div className="attribution">
+        Challenge by <a className="text-blue-600 no-underline hover:underline hover:text-blue-800" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a className="text-blue-600 no-underline hover:underline hover:text-blue-800" href="https://www.instagram.com/moataz7529/">Moataz Ahmed</a>.
+      </div>
     </>
   )
 }
